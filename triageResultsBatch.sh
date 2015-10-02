3b6e875887b05f4e004deaa60ad842ddf0ef0324
John Kerfoot <johnkerfoot@gmail.com>
2015-10-02 12:53:21 -0400

    resolved merge conflicts

--- triageResultsBatch.sh
+++ triageResultsBatch.sh
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-#! /bin/bash -x
-=======
 #! /bin/bash
->>>>>>> 2f62b041ebdbaaaa8b904145d6c865a60df0603b
 #
 # USAGE:
 #
@@ -47,7 +43,6 @@ done
 # Remove option from $@
 shift $((OPTIND-1));
 
-<<<<<<< HEAD
 TRIAGE_DEST_ROOT="${HOME}/triage/results";
 
 if [ ! -d "$TRIAGE_DEST_ROOT" ]
@@ -111,56 +106,5 @@ do
 	        $f > $out_file;
 
     done
-=======
-TRIAGE_DEST_ROOT="${HOME}/triage/parsed";
-
-if [ ! -d "$TRIAGE_DEST_ROOT" ]
-then
-    echo "Invalid triage destination directory: $TRIAGE_DEST_ROOT" >&2;
-    exit 1;
-fi
-
-for f in ${TRIAGE_DEST_ROOT}/*.parsed.json
-do
-
-    [ ! -f "$f" ] && continue;
-
-    echo '------------------------------------------------------------------------------';
-    echo "Inspecting: $f";
-
-    # Strip off the deployment name to create the output file
-    parsed_base=$(basename $f .parsed.json);
-    echo "Deployment: $parsed_base";
-
-    # Create the list of incompletely parsed files
-    out_file="${TRIAGE_DEST_ROOT}/${parsed_base}.incomplete.csv";
-    echo "Incomplete: $out_file";
-
-    inspect_ingest_performance.py \
-        -r \
-        $f > $out_file;
-
-    # Create list of completely parsed files with number of particles created
-    out_file="${TRIAGE_DEST_ROOT}/${parsed_base}.complete.csv";
-    echo "Complete: $out_file";
-
-    inspect_ingest_performance.py \
-        -c \
-        -p \
-        -d \
-        $f > $out_file;
-
-    # Create list of completely parsed files with parser errors and number of 
-    # particles created and parser name
-    out_file="${TRIAGE_DEST_ROOT}/${parsed_base}.complete-parser-errors.csv";
-    echo "Complete w/errors: $out_file";
-
-    inspect_ingest_performance.py \
-        -c \
-        -p \
-        -d \
-        --parser_errors \
-        $f > $out_file;
->>>>>>> 2f62b041ebdbaaaa8b904145d6c865a60df0603b
 
 done
